1880c28512f4fa18a647a3087e6cd987e691ad91
Kevin J. Lynagh <kevin@dirigibleFlightcraft.com>
2010-10-30 09:03:26 -0700

    R wrapper returns factor-expanded model correctly

--- R/Rliblinear.R
+++ R/Rliblinear.R
@@ -25,7 +25,7 @@ liblinear = function(
 
   if(!is.character(type))
     stop(type_err)
-  
+
   type = types[[type]]
   if(is.null(type))
     stop(type_err)
@@ -33,7 +33,7 @@ liblinear = function(
   # Nb samples
   n=dim(data)[1]
   # Nb features
-  p=dim(data)[2]
+  orig_dim=dim(data)[2]
 
 
   # Epsilon
@@ -96,7 +96,23 @@ liblinear = function(
     return(if (is.null(l)) 1 else length(l))
   })
 
-  
+  p = sum(p_levels)
+
+  #column names with factors expanded (e.g. variable 'a' with three levels will have colnames c('a_level1', 'a_level2', 'a_level3'))
+  data_colnames = unlist(sapply(1:orig_dim, function(i){
+    orig_colname = colnames(data)[i]
+    if(is.null(orig_colname))
+      orig_colname = paste('V', i, sep='')
+    if(p_levels[i] == 1)
+      orig_colname
+    else
+      paste(orig_colname, levels(data[,i]), sep=' = ')
+  }))
+    
+  if(bias) data_colnames = c(data_colnames, 'bias')
+
+  print(paste(data_colnames, collapse=' '))
+
   # Return storage preparation for result
   if(nbClass==2){
     if(bias){
@@ -117,15 +133,15 @@ liblinear = function(
   else{
     stop("Wrong number of classes ( < 2 ).\n")
   }
-  
-  
+
+
   # as.double(t(X)) corresponds to rewrite X as a nxp-long vector instead of a n-rows and p-cols matrix. Rows of X are appended one at a time.
   ret <- .C("trainLinear",
             as.double(W),
             as.double(t(data.matrix(data))),
             as.double(yC),
             as.integer(n), #the number of training data
-            as.integer(p), #the number of dimensions
+            as.integer(orig_dim), #the number of columns in the data frame
             as.integer(p_levels), #the number of levels for each dimension (1 for non-factors)
             as.integer(if(bias){1}else{-1}),
             as.integer(type),
@@ -137,43 +153,24 @@ liblinear = function(
             as.integer(cross),
             as.integer(verbose)
             )
-  print(str(ret))
-  print(ret)
-  
-  if(cross != 0){ #just return the cross validation accuracy reported by liblinear
-    return(ret[[1]][1])
-  }else{
-    if(nbClass==2){
-      w=matrix(nc=dim(W)[2],nr=1,data=ret[[1]])
-    }
-    else{
-      w=matrix(nc=dim(W)[2]/nbClass,nr=nbClass,data=ret[[1]],byrow=TRUE)
-    }
-    if(!is.null(colnames(data))){
-      if(bias){
-        colnames(w)=c(colnames(data),"Bias")
-      }
-      else{
-        colnames(w)=colnames(data)
-      }
-    }
-    else{
-      if(bias){
-        colnames(w)=c(paste("W",c(1:dim(data)[2]),sep=""),"Bias")
-      }
-      else{
-        colnames(w)=c(paste("W",c(1:dim(data)[2]),sep=""))
-      }
-    }
 
-    m=list()
-    class(m)="liblinear"
-    m$type_detail= names(Filter(function(x){x == type}, types))
-    m$type=type
-    m$w=w
-    m$bias=bias
-    m$class_names=yLev
-    m$nb_class=nbClass
-    return(m)
-  }
+  if(cross != 0) #just return the cross validation accuracy reported by liblinear
+    return(ret[[1]][1])
+  else
+    if(nbClass==2) #two class problems get a single weight vector for classification
+      w = matrix(nc=dim(W)[2],nr=1,data=ret[[1]])
+    else #multiclass problems get a weight vector for each class (one vs. all)
+      w=matrix(nc=dim(W)[2]/nbClass,nr=nbClass,data=ret[[1]],byrow=TRUE)
+
+  colnames(w) = data_colnames
+
+  m=list()
+  class(m)="liblinear"
+  m$type_detail= names(Filter(function(x){x == type}, types))
+  m$type=type
+  m$w=w
+  m$bias=bias
+  m$class_names=yLev
+  m$nb_class=nbClass
+  return(m)
 }
